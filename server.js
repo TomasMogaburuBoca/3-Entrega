2e55e32e81a7efac4c2f57f040be7289d4a8a8d6
Tomas Mogaburu Boca <tomasmogaburu@gmail.com>
2022-09-16 21:06:12 -0300

    Port

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const app = express();
 
 const container = new Container ('product.txt');
 const products = async() => {await container.getAll();}
-
 console.log(products);
 
 const appProducts = app.get ('/products', (req, res) => {
     res.send (`Our products are: ${products.then(prod => {prod})}`)
 })
 console.log(appProducts);
+
+
+const PORT = process.env.port || 8080;
+
+const server = app.listen(8080, (req, res) => {
+    console.log(`El puerto ${PORT} esta corriendo OK`);
+})
+console.log(server);
